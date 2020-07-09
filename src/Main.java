7cdb3b9d95013f2f627999dc7868b661b0742f2e
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-09 16:23:43 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * Created by KPS on 7/9/2020.
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input the first number : ");
+        int x = in.nextInt();
+        System.out.print("Input the second number: ");
+        int y = in.nextInt();
+        System.out.print("Input the third number : ");
+        int z = in.nextInt();
+        System.out.print("The result is: "+test(x, y, z,true));
+        System.out.print("\n");
+    }
+
+    public static boolean test(int p, int q, int r, boolean xyz)
+    {
+        if(xyz)
+            return (r > q);
+        return (q > p && r > q);
+    }
+}
